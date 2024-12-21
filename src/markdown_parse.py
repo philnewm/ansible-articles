208b1725e17b8f58923faf91e0248ae35bcd3687
PhilNewm <Philippkd8@gmail.com>
2024-12-21 20:12:55 +0100

    Move away from tokens for replacement

--- src/markdown_parse.py
+++ src/markdown_parse.py
@@ -15,28 +15,6 @@ def dict_to_object(name: str, dictionary: dict[str, str]):
 
 input_file = "./ansible_molecule/getting_started/Ansible Molecule using Vagrant & Virtualbox.md"
 
-# for token in tokens:
-#     if token.type == "fence" and token.info == "reference":
-#         yaml_content = yaml.safe_load(token.content)
-#         yaml_obj = dict_to_object(name="yamlObject", dictionary=yaml_content)
-#         file_reference: str = yaml_obj.file
-
-#         if file_reference.endswith(os.path.basename(workflow_path)):
-#             workflow_code: str = query_code_from_workflow(
-#                 file_name=workflow_path,
-#                 job_name="molecule-setup-ci",
-#                 step_name=yaml_obj.title
-#                 )
-
-# query code reference fields
-# get referenced file per field
-# if file is workflow look for steps with same name
-# -> replace code reference with step code
-# default: replace with referenced file content
-# update token values for code snippets
-# combine content back together
-# overwrite previous file
-
 def map_step_name_to_code(workflow_file: str, job_name: str) -> dict[str, str]:
     
     with open(workflow_file, "r") as file:
@@ -52,10 +30,29 @@ step_name: str = "Setup role and molecule scenario"
 
 md = MarkdownIt()
 
-with open(input_file, "r") as file:
-    md_content: str = file.read()
+in_ref_code_block = False
+code_block_yaml: str = ""
+updated_lines: list[str] = []
 
-tokens: list[Token] = md.parse(md_content)
+with open(input_file, "r") as file:
+    # md_content: str = file.read()
+    for line in file:
+        if line.startswith("```reference"):
+            in_ref_code_block = True
+            continue
+
+        if in_ref_code_block:
+            code_block_yaml += line
+
+        if line.startswith("```") and in_ref_code_block:
+            in_ref_code_block = False
+            updated_lines.append(f"```yaml\n{code_block_yaml}```")
+            code_block_yaml = ""
+            continue
+
+
+
+# tokens: list[Token] = md.parse(md_content)
 
 result:dict[str, str] = map_step_name_to_code(
     workflow_file=workflow_path,
@@ -64,35 +61,48 @@ result:dict[str, str] = map_step_name_to_code(
 
 # updated_md_content: str = "".join(token.content if token.content else token.markup + "\n" for token in tokens)
 
-reconstructed_content = ""
+# reconstructed_content: str = ""
 
-for index, token in enumerate(tokens):
-    token: Token = tokens[index]
-    if token.type == "heading_open":
-        if token.tag == "h2":
-            reconstructed_content += "\n"
-        heading: str = f"{token.markup} {tokens[index + 1].content} {tokens[index + 2].markup}\n"
-        reconstructed_content += heading
-        continue
+# for index, token in enumerate(tokens):
+#     if token.type == "heading_close":
+#         if token.tag != "h1":
+#             reconstructed_content += "\n"
+ 
+#         heading: str = f"{tokens[index - 2].markup} {tokens[index - 1].content}\n\n"
+#         reconstructed_content += heading
+#         continue
 
-    if token.type == "fence":
-        reconstructed_content += f"\n```{token.info}\n{token.content}```\n\n"
-        continue
+#     if token.type == "fence":
+#         reconstructed_content += f"\n```{token.info}\n{token.content}```\n\n"
+#         continue
 
-    if token.type == "paragraph_open" and tokens[index-1].type != "list_item_open":
-        reconstructed_content += "\n"
+#     if token.type == "paragraph_close":
+#         reconstructed_content += "\n"
+#         continue
 
-    if token.type == "paragraph_close" and tokens[index-1].type != "list_item_close":
-        reconstructed_content += "\n"
+#     if token.type == "list_item_open":
+#         reconstructed_content += f"{token.markup} {tokens[index+2].content}"
+#         continue
 
-    if token.type == "bullet_list_open":
-        reconstructed_content += "\n"
+#     if token.type == "bullet_list_open":
+#         reconstructed_content += "\n"
+#         continue
 
-    if token.type == "list_item_open":
-        list_item: str = f"{token.markup} {tokens[index + 2].content}"
-        continue
+#     if token.type == "bullet_list_close":
+#         reconstructed_content += "\n"
+#         continue
 
-    if token.content and tokens[index-1].type != "heading_open":
-        reconstructed_content += f"{token.content}"
+#     if token.content:
+#         if tokens[index-1].type == "heading_open":
+#             continue
 
-print(reconstructed_content)
+#         if tokens[index-2].type == "list_item_open":
+#             continue
+
+#         reconstructed_content += f"{token.content}"
+
+# print(reconstructed_content)
+
+# output_file = "output.md"
+# with open(output_file, "w") as file:
+#     file.write(reconstructed_content)
